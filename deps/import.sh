9f595ba7e691a4544496e6ea66e40b29c1c04ee7
Vanilla Hsu <v@fatpipi.com>
2011-03-20 00:13:35 +0800

    import libmemcached 0.48.

--- deps/import.sh
+++ deps/import.sh
@@ -2,10 +2,17 @@
 
 SOURCE_DIR=$1
 
-mkdir libmemcached
-mkdir libhashkit
-mkdir libmemcached/memcached
+if [ ! -d libmemcached ]; then
+    mkdir libmemcached
+fi
+if [ ! -d libhashkit ]; then
+    mkdir libhashkit
+fi
+if [ ! -d libmemcached/memcached ]; then
+    mkdir libmemcached/memcached
+fi
 
-cp $SOURCE_DIR/libhashkit/*.[ch] libhashkit/
-cp $SOURCE_DIR/libmemcached/*.[ch] libmemcached/
-cp $SOURCE_DIR/libmemcached/memcached/*.h libmemcached/memcached
+cp -f $SOURCE_DIR/libhashkit/*.[ch] libhashkit/
+cp -f $SOURCE_DIR/libmemcached/*.[ch] libmemcached/
+cp -f $SOURCE_DIR/libmemcached/memcached/*.h libmemcached/memcached
+rm libmemcached/sasl.c
